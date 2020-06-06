4fb51c5f58e0ff9c1c7a78cd84e67fa757b372bf
dejandokic <doxa997@gmail.com>
2020-06-06 18:19:35 +0200

    early game part 2 test

--- backend/src/main/java/com/siit/sbnz/sbnztim14/model/AllyChampion.java
+++ backend/src/main/java/com/siit/sbnz/sbnztim14/model/AllyChampion.java
@@ -11,4 +11,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AllyChampion extends Champion {
     private String playType = "aggro";
+
+    public AllyChampion(Champion cha, String playType) {
+        super(cha.getName(), cha.getLane(), cha.getRole(), cha.isGoodEarly(), cha.isGoodMid(), cha.isGoodLate(),
+                cha.isHardCC(), cha.isHardEngage(), cha.isDisengage(), cha.isPoke(), cha.isWaveclear(),
+                cha.isSustain(), cha.isUtility(), cha.isMobility(), cha.isAoeDamage(), cha.isSplitPush(),
+                cha.getGoodAgainst(), cha.getBadAgainst(), cha.getAllPossibleLanes());
+        this.playType = playType;
+    }
 }
