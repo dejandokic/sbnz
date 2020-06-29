aab174f7b1ff5919052bea0d46cadad42f119aa7
Dz0n1Pr0 <ignjatovicn97@gmail.com>
2020-06-29 22:59:50 +0200

    Updated event

--- backend/src/main/java/com/siit/sbnz/sbnztim14/model/Event.java
+++ backend/src/main/java/com/siit/sbnz/sbnztim14/model/Event.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.kie.api.definition.type.Expires;
+import org.kie.api.definition.type.Role;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Role(Role.Type.EVENT)
+@Expires("10m")
 public class Event {
 
     private String lane;
