c4a612d414853bf8e1c9b2668349d1f92d49d547
Dz0n1Pr0 <ignjatovicn97@gmail.com>
2020-07-01 22:04:38 +0200

    Item set test

--- backend/src/test/java/com/siit/sbnz/sbnztim14/ItemsTest.java
+++ backend/src/test/java/com/siit/sbnz/sbnztim14/ItemsTest.java
@@ -12,8 +12,10 @@ import org.kie.api.runtime.rule.FactHandle;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ItemsTest {
 
@@ -33,58 +35,143 @@ public class ItemsTest {
 
         KieSession kSession = kContainer.newKieSession("ksession-rules");
 
-        // Choose 10 champs
-        Champion champion1 = championService.getChampionByName("Annie");
-        champion1.setLane("mid");
-        Champion champion2 = championService.getChampionByName("Akali");
-        champion2.setLane("mid");
+        Champion champion1 = championService.getChampionByName("Darius");
+        champion1.setLane("top");
+        Champion champion2 = championService.getChampionByName("Nasus");
+        champion2.setLane("top");
+        Champion champion3 = championService.getChampionByName("Rengar");
+        champion3.setLane("jungle");
+        Champion champion4 = championService.getChampionByName("Sejuani");
+        champion4.setLane("jungle");
+        Champion champion5 = championService.getChampionByName("Annie");
+        champion5.setLane("mid");
+        Champion champion6 = championService.getChampionByName("Lux");
+        champion6.setLane("mid");
+        Champion champion7 = championService.getChampionByName("Vayne");
+        champion7.setLane("adc");
+        Champion champion8 = championService.getChampionByName("Varus");
+        champion8.setLane("adc");
+        Champion champion9 = championService.getChampionByName("Zilean");
+        champion9.setLane("support");
+        Champion champion10 = championService.getChampionByName("Braum");
+        champion10.setLane("support");
 
+        // Ally team
         AllyChampion ally1 = new AllyChampion(champion1, "aggro");
+        AllyChampion ally2 = new AllyChampion(champion3, "aggro");
+        AllyChampion ally3 = new AllyChampion(champion5, "def");
+        AllyChampion ally4 = new AllyChampion(champion7, "aggro");
+        AllyChampion ally5 = new AllyChampion(champion9, "def");
 
+        // Enemy team
         EnemyChampion enemy1 = new EnemyChampion(champion2);
-
+        EnemyChampion enemy2 = new EnemyChampion(champion4);
+        EnemyChampion enemy3 = new EnemyChampion(champion6);
+        EnemyChampion enemy4 = new EnemyChampion(champion8);
+        EnemyChampion enemy5 = new EnemyChampion(champion10);
 
         ItemBuy itemBuy1 = new ItemBuy();
-        itemBuy1.setLane("mid");
+        itemBuy1.setLane("top");
         itemBuy1.setValue(0);
+        ItemBuy itemBuy2 = new ItemBuy();
+        itemBuy2.setLane("jungle");
+        itemBuy2.setValue(0);
+        ItemBuy itemBuy3 = new ItemBuy();
+        itemBuy3.setLane("mid");
+        itemBuy3.setValue(0);
+        ItemBuy itemBuy4 = new ItemBuy();
+        itemBuy4.setLane("adc");
+        itemBuy4.setValue(0);
+        ItemBuy itemBuy5 = new ItemBuy();
+        itemBuy5.setLane("support");
+        itemBuy5.setValue(0);
 
+
+
+
+        for(Item it: itemService.getAllItems()){
+            //Wanted item already set
+            if(it.getName().equals("Mercurial Scimitar")){
+                ally2.setWantedItem(it);
+            }else if(it.getName().equals("Spirit Visage")){
+                ally5.setWantedItem(it);
+            }
+            //Item already bought for top
+            if(it.getName().equals("Infinity Edge")){
+                ArrayList<Item> list = new ArrayList<>();
+                list.add(it);
+                ally1.setBought(list);
+            }
+        }
+
+
+        // Insert ally
         kSession.insert(ally1);
-        kSession.insert(enemy1);
-        kSession.insert(itemBuy1);
+        kSession.insert(ally2);
+        kSession.insert(ally3);
+        kSession.insert(ally4);
+        kSession.insert(ally5);
 
+        // Insert enemy
+        kSession.insert(enemy1);
+        kSession.insert(enemy2);
+        kSession.insert(enemy3);
+        kSession.insert(enemy4);
+        kSession.insert(enemy5);
+
+        // Insert item points
+        kSession.insert(itemBuy1);
+        kSession.insert(itemBuy2);
+        kSession.insert(itemBuy3);
+        kSession.insert(itemBuy4);
+        kSession.insert(itemBuy5);
+
+        // Insert items
         for(Item it: itemService.getAllItems()){
             kSession.insert(it);
         }
 
 
-        kSession.insert(new GameEvent("mid", ally1, enemy1, EventType.ALLY_KILLS));
-        kSession.insert(new GameEvent("mid", ally1, enemy1, EventType.ALLY_KILLS));
-        kSession.insert(new GameEvent("mid", ally1, enemy1, EventType.ALLY_KILLS));
-        kSession.insert(new GameEvent("mid", ally1, enemy1, EventType.ALLY_KILLS));
+        kSession.insert(new GameEvent("top", ally1, enemy1, EventType.ALLY_KILLS));
+        kSession.insert(new GameEvent("top", ally1, enemy1, EventType.ALLY_KILLS));
+        kSession.insert(new GameEvent("top", ally1, enemy1, EventType.ALLY_KILLS));
+
+        kSession.insert(new GameEvent("jungle", ally2, enemy2, EventType.ALLY_KILLS));
+        kSession.insert(new GameEvent("jungle", ally2, enemy2, EventType.ALLY_KILLS));
+
+        kSession.insert(new GameEvent("mid", ally3, enemy3, EventType.ALLY_KILLS));
+        kSession.insert(new GameEvent("mid", ally3, enemy3, EventType.ALLY_KILLS));
+
+        kSession.insert(new GameEvent("adc", ally4, enemy4, EventType.ENEMY_KILLS));
+        kSession.insert(new GameEvent("adc", ally4, enemy4, EventType.ENEMY_KILLS));
+        kSession.insert(new GameEvent("adc", ally4, enemy4, EventType.ALLY_KILLS));
+
+        kSession.insert(new GameEvent("support", ally5, enemy5, EventType.ENEMY_KILLS));
 
 
         kSession.getAgenda().getAgendaGroup("items").setFocus();
         int valu = kSession.fireAllRules();
 
+        assertEquals(9, valu);
 
-        ArrayList<Item> i = new ArrayList<>();
+        assertEquals("Infinity Edge2",ally1.getWantedItem().getName());
 
-        for(Item it: itemService.getAllItems()){
-            if(it.getName().equals("Rabadons Deathcap")){
-                i.add(it);
-            }
-        }
+        assertEquals(0,ally2.getBought().size());
 
-        ally1.setBought(i);
+        assertEquals("Banshees Veil",ally3.getWantedItem().getName());
 
-        ally1.setWantedItem(null);
+        assertEquals("Randuins Omen",ally4.getWantedItem().getName());
 
-        System.out.println(ally1.getWantedItem() + " dada");
 
-        kSession.getAgenda().getAgendaGroup("items").setFocus();
-        int valu2 = kSession.fireAllRules();
+        assertEquals(0,ally5.getBought().size());
 
-        System.out.println(valu + " - " + valu2);
+//        Collection objects = kSession.getObjects();
+//        for (Object fact : objects )
+//        {
+//            FactHandle factHandle = kSession.getFactHandle(fact);
+//            kSession.update(factHandle, fact);
+//        }
+
 
     }
 }
